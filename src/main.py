09331ab4bb822ef73a96c82b0d3d629b282f1ef5
hairline-bandit <77307697+hairline-bandit@users.noreply.github.com>
2021-04-18 19:43:20 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,20 +7,104 @@ def push(name, value):
 def remove(name, index):
     new_f.write(name + " = append(" + name + "[:" + index + "], " + name + "[" + index + " + 1:]...)\n")
 
+def strIndex(name, string, vardump):
+    new_f.write("var " + vardump + " int = " + "strings.Index(" + name + ", " + string + ")\n")
+
+def strRindex(name, string, vardump):
+    new_f.write("var " + vardump + " int = " + "strings.LastIndex(" + name + ", " + string + ")\n")
+
+def split(name, delim, vardump, tabs):
+    new_f.write("var " + vardump + " []string\n" + "\t" * tabs + vardump + " = strings.Split(" + name + ", " + delim + ")\n")
+
+def join(name, sep, vardump):
+    new_f.write(vardump + " := strings.Join(" + name + ", " + sep + ")\n")
+
+def arrIndexS(name, value, vardump):
+    new_f.write(vardump + " := arrIndexS(" + name + ", " + value + ")\n")
+
+def arrIndexI(name, value, vardump):
+    new_f.write(vardump + " := arrIndexI(" + name + ", " + value + ")\n")
+
+def arrIndexF(name, value, vardump):
+    new_f.write(vardump + " := arrIndexF(" + name + ", " + value + ")\n")
+
+def arrIndexC(name, value, vardump):
+    new_f.write(vardump + " := arrIndexC(" + name + ", " + value + ")\n")
+
+def arrIndexB(name, value, vardump):
+    new_f.write(vardump + " := arrIndexB(" + name + ", " + value + ")\n")
+
+def arrRindexS(name, value, vardump):
+    new_f.write(vardump + " := arrRindexS(" + name + ", " + value + ")\n")
+
+def arrRindexI(name, value, vardump):
+    new_f.write(vardump + " := arrRindexI(" + name + ", " + value + ")\n")
+
+def arrRindexF(name, value, vardump):
+    new_f.write(vardump + " := arrRindexF(" + name + ", " + value + ")\n")
+
+def arrRindexC(name, value, vardump):
+    new_f.write(vardump + " := arrRindexC(" + name + ", " + value + ")\n")
+
+def arrRindexB(name, value, vardump):
+    new_f.write(vardump + " := arrRindexB(" + name + ", " + value + ")\n")
+
 import sys
 
 file = sys.argv[1]
 types = ["int", "str", "flt", "char", "bool", "[]int", "[]str", "[]flt", "[]char", "[]bool"]
 func_types = ["int", "str", "flt", "char", "bool", "[]int", "[]str", "[]flt", "[]char", "[]bool", "null"]
 ifs = ["if", "else", "nor"]
-built_ins = ["pop:", "push:", "remove:"]
+built_ins = ["pop:", "push:", "remove:", "strIndex:", "strRindex:", "split:", "join:", "arrIndexS:", "arrRindexS:",
+"arrIndexI:", "arrRindexI:", "arrIndexF:", "arrRindexF:", "arrIndexC:", "arrRindexC:", "arrIndexB:", "arrRindexB:"]
 defed_funcs = []
+
+needed_imports = ["\"fmt\""]
+needed_funcs = []
 
 with open(file, "r") as f:
     code_lines = [i.replace("\n", "") for i in f.readlines()]
 
+looking = "".join(code_lines)
+
+if "> strIndex: " in looking:
+    needed_imports.append("\"strings\"")
+elif "> strRindex: " in looking:
+    needed_imports.append("\"strings\"")
+elif "> split: " in looking:
+    needed_imports.append("\"strings\"")
+elif "> join: " in looking:
+    needed_imports.append("\"strings\"")
+
+if "> arrIndexS: " in looking:
+    needed_funcs.append("arrIndexS")
+if "> arrRindexS: " in looking:
+    needed_funcs.append("arrRindexS")
+if "> arrIndexI: " in looking:
+    needed_funcs.append("arrIndexI")
+if "> arrRindexI: " in looking:
+    needed_funcs.append("arrRindexI")
+if "> arrIndexF: " in looking:
+    needed_funcs.append("arrIndexF")
+if "> arrRindexF: " in looking:
+    needed_funcs.append("arrRindexF")
+if "> arrIndexC: " in looking:
+    needed_funcs.append("arrIndexC")
+if "> arrRindexC: " in looking:
+    needed_funcs.append("arrRindexC")
+if "> arrIndexB: " in looking:
+    needed_funcs.append("arrIndexB")
+if "> arrRindexB: " in looking:
+    needed_funcs.append("arrRindexB")
+
 new_f = open("123123123123.go", "w")
-new_f.write("package main\nimport (\n\t\"fmt\"\n)\n")
+
+# new_f.write("package main\nimport (\n\t\"fmt\"\n)\n") <- removed as built in functions need golang imports
+new_f.write("package main\nimport(")
+for i in needed_imports:
+    new_f.write("\n\t" + i)
+new_f.write("\n)\n")
+
 for line in enumerate(code_lines):
     # whitespace
     if line[1] == "":
@@ -294,9 +378,11 @@ for line in enumerate(code_lines):
 
     # built ins
     elif len(line[1].split(" ")) > 1 and line[1].split(" ")[1] in built_ins:
+        tabs = 0
         for i in line[1].split(" ")[0]:
             if i == ">":
                 new_f.write("\t")
+                tabs += 1
             elif i == "<":
                 new_f.write("}")
         if line[1].split(" ")[1] == "pop:":
@@ -310,6 +396,76 @@ for line in enumerate(code_lines):
             name = line[1].split(" ")[2][:-1]
             index = line[1].split(" ")[3][:-1]
             remove(name, index)
+        elif line[1].split(" ")[1] == "strIndex:":
+            name = line[1].split(" ")[2][:-1]
+            string = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            strIndex(name, string, vardump)
+        elif line[1].split(" ")[1] == "strRindex:":
+            name = line[1].split(" ")[2][:-1]
+            string = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            strRindex(name, string, vardump)
+        elif line[1].split(" ")[1] == "split:":
+            name = line[1].split(" ")[2][:-1]
+            delim = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            split(name, delim, vardump, tabs)
+        elif line[1].split(" ")[1] == "join:":
+            name = line[1].split(" ")[2][:-1]
+            sep = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            join(name, sep, vardump)
+        elif line[1].split(" ")[1] == "arrIndexS:":
+            name = line[1].split(" ")[2][:-1]
+            value = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            arrIndexS(name, value, vardump)
+        elif line[1].split(" ")[1] == "arrRindexS:":
+            name = line[1].split(" ")[2][:-1]
+            value = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            arrRindexS(name, value, vardump)
+        elif line[1].split(" ")[1] == "arrIndexI:":
+            name = line[1].split(" ")[2][:-1]
+            value = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            arrIndexI(name, value, vardump)
+        elif line[1].split(" ")[1] == "arrRindexI:":
+            name = line[1].split(" ")[2][:-1]
+            value = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            arrRindexI(name, value, vardump)
+        elif line[1].split(" ")[1] == "arrIndexF:":
+            name = line[1].split(" ")[2][:-1]
+            value = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            arrIndexF(name, value, vardump)
+        elif line[1].split(" ")[1] == "arrRindexF:":
+            name = line[1].split(" ")[2][:-1]
+            value = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            arrRindexF(name, value, vardump)
+        elif line[1].split(" ")[1] == "arrIndexC:":
+            name = line[1].split(" ")[2][:-1]
+            value = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            arrIndexC(name, value, vardump)
+        elif line[1].split(" ")[1] == "arrRindexC:":
+            name = line[1].split(" ")[2][:-1]
+            value = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            arrRindexC(name, value, vardump)
+        elif line[1].split(" ")[1] == "arrIndexB:":
+            name = line[1].split(" ")[2][:-1]
+            value = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            arrIndexB(name, value, vardump)
+        elif line[1].split(" ")[1] == "arrRindexB:":
+            name = line[1].split(" ")[2][:-1]
+            value = line[1][line[1].index(",") + 2: line[1].rindex(",")]
+            vardump = line[1].split(" ")[-1][:-1]
+            arrRindexB(name, value, vardump)
 
 
     # change var values
@@ -332,5 +488,27 @@ for line in enumerate(code_lines):
                 new_f.write("}")
         new_f.write("\n")
 
+if len(needed_funcs) > 0:
+    for i in needed_funcs:
+        if i == "arrIndexS":
+            new_f.write("func arrIndexS(slice []string, target string) int {\n\tfor i, v := range slice {\n\t\tif v == target {\n\t\t\treturn i\n\t\t}\n\t}\n\treturn -1\n}\n")
+        elif i == "arrRindexS":
+            new_f.write("func arrRindexS(slice []string, target string) int {\n\tfor i := len(slice) - 1; i >= 0; i-- {\n\t\tif slice[i] == target {\n\t\t\treturn i\n\t\t}\n\t}\n\treturn -1\n}\n")
+        elif i == "arrIndexI":
+            new_f.write("func arrIndexI(slice []int, target int) int {\n\tfor i, v := range slice {\n\t\tif v == target {\n\t\t\treturn i\n\t\t}\n\t}\n\treturn -1\n}\n")
+        elif i == "arrRindexI":
+            new_f.write("func arrRindexI(slice []int, target int) int {\n\tfor i := len(slice) - 1; i >= 0; i-- {\n\t\tif slice[i] == target {\n\t\t\treturn i\n\t\t}\n\t}\n\treturn -1\n}\n")
+        elif i == "arrIndexF":
+            new_f.write("func arrIndexF(slice []float64, target float64) int {\n\tfor i, v := range slice {\n\t\tif v == target {\n\t\t\treturn i\n\t\t}\n\t}\n\treturn -1\n}\n")
+        elif i == "arrRindexF":
+            new_f.write("func arrRindexF(slice []float64, target float64) int {\n\tfor i := len(slice) - 1; i >= 0; i-- {\n\t\tif slice[i] == target {\n\t\t\treturn i\n\t\t}\n\t}\n\treturn -1\n}\n")
+        elif i == "arrIndexC":
+            new_f.write("func arrIndexC(slice []rune, target rune) int {\n\tfor i, v := range slice {\n\t\tif v == target {\n\t\t\treturn i\n\t\t}\n\t}\n\treturn -1\n}\n")
+        elif i == "arrRindexC":
+            new_f.write("func arrRindexC(slice []rune, target rune) int {\n\tfor i := len(slice) - 1; i >= 0; i-- {\n\t\tif slice[i] == target {\n\t\t\treturn i\n\t\t}\n\t}\n\treturn -1\n}\n")
+        elif i == "arrIndexB":
+            new_f.write("func arrIndexB(slice []bool, target bool) int {\n\tfor i, v := range slice {\n\t\tif v == target {\n\t\t\treturn i\n\t\t}\n\t}\n\treturn -1\n}\n")
+        elif i == "arrRindexB":
+            new_f.write("func arrRindexB(slice []bool, target bool) int {\n\tfor i := len(slice) - 1; i >= 0; i-- {\n\t\tif slice[i] == target {\n\t\t\treturn i\n\t\t}\n\t}\n\treturn -1\n}\n")
 
 new_f.close()
